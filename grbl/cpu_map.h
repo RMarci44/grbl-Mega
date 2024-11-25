5f0c79c06d5edb08dc49e9f442907ab5b43c30cd
rm44 <reveszmarci.rm@gmail.com>
2024-11-25 10:51:35 +0100

    spindle on D6

--- grbl/cpu_map.h
+++ grbl/cpu_map.h
@@ -268,28 +268,29 @@
   #define SPINDLE_PWM_RANGE         (SPINDLE_PWM_MAX_VALUE-SPINDLE_PWM_MIN_VALUE)
 
   //Control Digital Pin 6 which is Servo 2 signal pin on Ramps 1.4 board
-  //#define SPINDLE_TCCRA_REGISTER    TCCR4A 
-  #define SPINDLE_TCCRA_REGISTER    TCCR2A 
-  //#define SPINDLE_TCCRB_REGISTER    TCCR4B
-  #define SPINDLE_TCCRB_REGISTER    TCCR2B
+  #define SPINDLE_TCCRA_REGISTER    TCCR4A 
+  #define SPINDLE_TCCRB_REGISTER    TCCR4B
   //#define SPINDLE_OCR_REGISTER      OCR4C //D8
-  #define SPINDLE_OCR_REGISTER      OCR2A //Timer 2 for PB4 - Pin 10
+  #define SPINDLE_OCR_REGISTER      OCR4A //Timer 4A for Pin 6
   //#define SPINDLE_COMB_BIT          COM4C1
-  #define SPINDLE_COMB_BIT          COM2A1
+  #define SPINDLE_COMB_BIT          COM4A1
 
-  // 1/8 Prescaler, 16-bit Fast PWM mode
+  // 1/8 Prescaler, 8-bit Fast PWM mode
   // #define SPINDLE_TCCRA_INIT_MASK ((1<<WGM40) | (1<<WGM41))
-  #define SPINDLE_TCCRA_INIT_MASK ((1<<WGM20) | (1<<WGM21))
+  #define SPINDLE_TCCRA_INIT_MASK ((1<<WGM41))
   //#define SPINDLE_TCCRB_INIT_MASK ((1<<WGM42) | (1<<WGM43) | (1<<CS41))
-  #define SPINDLE_TCCRB_INIT_MASK ((1<<CS22)) 
+  #define SPINDLE_TCCRB_INIT_MASK ((1<<WGM42) | (1<<WGM43) | (1<<CS41))
+  
+  #define SPINDLE_OCRA_REGISTER   ICR4 // 8-bit Fast PWM mode requires top reset value stored here.
+  #define SPINDLE_OCRA_TOP_VALUE  0xFF // PWM counter reset value. Should be the same as PWM_MAX_VALUE in hex.
 
   // Define spindle output pins.
   //#define SPINDLE_PWM_DDR   DDRH
-  #define SPINDLE_PWM_DDR   DDRB
+  #define SPINDLE_PWM_DDR   DDRH
   //#define SPINDLE_PWM_PORT  PORTB
-  #define SPINDLE_PWM_PORT  PORTB
+  #define SPINDLE_PWM_PORT  PORTH
   //#define SPINDLE_PWM_BIT   5 // MEGA2560 Digital Pin 8
-  #define SPINDLE_PWM_BIT   4 // MEGA2560 Digital Pin 10
+  #define SPINDLE_PWM_BIT   3 // MEGA2560 Digital Pin 6
 
 #endif
 /* 
